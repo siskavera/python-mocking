1bb58ff1c34c91a571dbffcff07728f3527fa09f
Veronika Siska <veronika.siska@tngtech.com>
2019-10-08 13:17:58 +0200

    Last adjustments

--- python-mocking/examples/pitfalls/weekday.py
+++ python-mocking/examples/pitfalls/weekday.py
@@ -5,7 +5,7 @@ import requests as requests
 def is_weekday():
     today = datetime.today()
     # Python's datetime library treats Monday as 0 and Sunday as 6
-    return (0 <= today.weekday() < 5)
+    return 0 <= today.weekday() < 5
 
 
 def greet_if_weekday():
